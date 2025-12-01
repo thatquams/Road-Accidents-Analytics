0a68e8c2becfcd38742fb3b7ab5a48d0ce7d2325
thatquams <devquams@gmail.com>
2025-12-01 13:50:29 +0100

    update

--- pages/predictions_page.py
+++ pages/predictions_page.py
@@ -5,7 +5,7 @@ import joblib
 from utils.combine_data import road_accidents_csv
 import os
 
-MODEL_PATH = os.path.join(os.path.dirname(__file__), "utils/data/svc_best_model.pkl")
+MODEL_PATH = os.path.join(os.path.dirname(__file__), "utils/data/gradient_boosting.pkl")
 STATE_ENCODER_PATH = os.path.join(os.path.dirname(__file__), "utils/data/state_encoder.pkl")
 SCALER_PATH = os.path.join(os.path.dirname(__file__), "utils/data/scaler.pkl")
 
