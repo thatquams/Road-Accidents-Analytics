50eaf34aeeec999d334bd09b36af22132c71a831
thatquams <devquams@gmail.com>
2025-12-02 22:30:42 +0100

    update

--- pages/utils/combine_data.py
+++ pages/utils/combine_data.py
@@ -3,20 +3,10 @@ import pandas as pd
 
 DATA_PATH = os.path.join(os.path.dirname(__file__), "data", "cleaned_accidents_data.csv")
 
-# def combine_data(on: str = "STATE", how: str = "left"):
-#     """Combines weather and state coordinates datasets."""
-#     df1 = pd.read_csv(os.path.join(os.path.dirname(__file__), "data", "weather_accidents.csv"))
-#     df2 = pd.read_csv(os.path.join(os.path.dirname(__file__), "data", "state_coordinates.csv"))
-    
-#     combined_df = pd.merge(df1, df2, on=on, how=how)
-#     combined_df.to_csv(DATA_PATH, index=False)
-#     return combined_df
 
 def read_accidents_data():
     """Reads the cleaned accidents data from CSV."""
-    # If file doesn't exist, create it
-    # if not os.path.exists(DATA_PATH):
-    #     combine_data()
+
     data = pd.read_csv(DATA_PATH)
     
     renamed_df = data.rename({"SPV":"Speed Violation", "UPD":"Use of Phone While Driving",
